a88ce1a367772e26887b005608c082330bbd75b5
Meriem1551 <meriem200409@gmail.com>
2023-09-16 15:33:06 +0200

    update project

--- src/main/java/org/arithExp/Main.java
+++ src/main/java/org/arithExp/Main.java
@@ -2,6 +2,25 @@ package org.arithExp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //ArithmeticException: +, -, *, /, %:
+
+        int result1 = 10 / 3; // the result is 3
+        double result2 = (double)10 / (double)3;//the result is 3.333333..5;
+        int x = 1;
+        int y = x++; // assign y to the origin al value of x and then increase x
+        // y = 1 & x = 2;
+        int z = ++x; // increase x and then assign it to z
+        // z = 2 & x = 2
+        x = x + 2;
+        //same as:
+        x += 2;// same for * - / %
+
+        /*
+          ORDER OF OPERATIONS:
+              ()
+             * /
+             + -
+         */
+
     }
 }
